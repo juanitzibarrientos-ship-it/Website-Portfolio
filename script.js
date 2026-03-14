3c28c33b61b3e7c8245e832b480f783e9b356ada
juanitzibarrientos-ship-it <juanitzibarrientos@gmail.com>
2026-03-14 22:27:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -369,3 +369,32 @@ this.reset();
 });
 
 }s.forEach(fader => appearOnScroll.observe(fader));
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+const updateCounter = () => {
+
+const target = +counter.getAttribute('data-target');
+const count = +counter.innerText;
+
+const increment = target / 200;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count + increment);
+
+setTimeout(updateCounter,10);
+
+}else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
+});
